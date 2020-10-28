dc883d1bead471167ecb820f00dd25b7e2e09031
Firas Naber <naber024@morris.umn.edu>
2020-10-28 16:25:58 -0500

    Implement EchoClient

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -15,9 +15,69 @@ public class EchoClient {
 
 	private void start() throws IOException {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+		// InputStream socketInputStream = socket.getInputStream();
+		// OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+		InputReader reader = new InputReader(socket);
+		OutputWriter writer = new OutputWriter(socket);
+		ThreadReader treader = new ThreadReader(reader);
+		ThreadWriter twriter = new ThreadWriter(writer);
+
+		treader.start();
+		twriter.start();
+
+	}
+
+	public class InputReader implements Runnable {
+		InputStream input;
+		Socket sock;
+
+		public InputReader(Socket sock) throws IOException {
+			this.sock = sock;
+			this.input = sock.getInputStream();
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			int inputByte;
+			try {
+				while ((inputByte = input.read()) != -1) {
+					System.out.write(inputByte);
+					System.out.flush();
+				}
+				sock.shutdownOutput();
+			} catch (IOException ioe) {
+				System.out.println("We caught an exception");
+				System.out.println(ioe);
+		}
+	
+	}
+	}
+
+	public class OutputWriter implements Runnable {
+		OutputStream output;
+		Socket sock;
+
+		public OutputWriter(Socket sock) throws IOException {
+			this.sock = sock;
+			this.output = sock.getOutputStream();
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			int outputByte;
+			try {
+				while ((outputByte = System.in.read()) != -1) {
+					output.write(outputByte);
+					output.flush();
+				}
+				sock.shutdownInput();
+			} catch (IOException ioe) {
+				System.out.println("We caught an exception");
+				System.out.println(ioe);
+			}
+		}
 	}
 }
